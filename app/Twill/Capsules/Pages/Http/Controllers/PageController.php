b1d9befe47026260e571a7c87c2e184aa6ba12e6
Harings Rob <haringsrob@gmail.com>
2021-11-17 09:31:25 +0100

    Add description to page table.

--- app/Twill/Capsules/Pages/Http/Controllers/PageController.php
+++ app/Twill/Capsules/Pages/Http/Controllers/PageController.php
@@ -9,4 +9,15 @@ class PageController extends ModuleController
     protected $moduleName = 'pages';
 
     protected $permalinkBase = '';
+
+    protected $indexColumns = [
+        'title' => [
+            'title' => 'Title',
+            'field' => 'title',
+        ],
+        'description' => [
+            'title' => 'Description',
+            'field' => 'description',
+        ],
+    ];
 }
